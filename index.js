fbb4cf9bbe51d0884f364fbc86b025d00688945d
Atharva Kamble <hitechatharva@gmail.com>
2023-10-13 20:13:44 +0530

    First Part

---
+++ index.js
@@ -0,0 +1,30 @@
+todomain();
+
+function todomain() {
+    let inputElem, ulElem;
+    getElements();
+    addListeners();
+
+    function getElements() {
+        inputElem = document.getElementsByTagName("input")[0]; // Get the first input element
+        ulElem = document.getElementsByTagName("ul")[0]; // Get the first ul element
+    }
+
+    function addListeners() {
+        inputElem.addEventListener("change", onChange, false);
+    }
+
+    function onChange(event) {
+        let inputval = this.value;//if not workin use inputElem.value
+
+        // ulElem.innerHTML += `<li>${inputval}</li>`; // Use inputval instead of e.target.value
+
+        console.log(inputval);
+        inputElem.value = ''; // Clear the input field after adding the item
+
+        let liElem = document.createElement("li");
+        liElem.innerHTML = inputval;
+        
+        ulElem.appendChild(liElem);
+    }
+}
